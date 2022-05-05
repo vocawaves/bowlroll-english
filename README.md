52f93948dc35dae450b059f39fa2045ba0b6d9fd
David Ralph <me@davidcralph.co.uk>
2022-05-05 22:52:52 +0100

    upload files

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # bowlroll-english
 Experimental user script and extension to translate BowlRoll into English
+
+## About
+Since BowlRoll has no English support and Google translating pages takes time + isn't very good, I decided to make this to instantly translate all of the user interface. The text is as accurate
+as DeepL can get with some minor edits for things to make more sense, without changing the meaning.
+
+## Download
+User script is in the repository, I'll get this on extension stores etc when I can.
+
+## What's not supported
+* Most if not all of the files index page, this is a limitation with the script due to BowlRoll's file index using JavaScript to request content on the client side
+* Same as above for the file pages themselves, although it is easier to navigate
+* Privacy Policy and Terms of Use pages - this is more of a legal thing as I feel I cannot make an accurate translation
+* About BowlRoll - translating this isn't particularly useful, but I might have a go later on
+* Contact Us - BowlRoll support likely won't respond to responses not in Japanese, therefore there is no use translating this page. This and the other 3 pages all have "(Japanese only)" next to their links, 
+similar to [niconico](https://nicovideo.jp/)'s English interface
+* Report file page - same as above
+* Probably file editing pages as I haven't uploaded any content myself, feel free to submit this
+* R-18 pages - this is out of scope for the project as VOCA-UK has no use for it and I doubt it'd be allowed on extension stores
+
+## License
+This is more of a BowlRoll thing, as I'm unsure of the licensing regarding its text content. Most of the content uploaded seems to go by a license that follows the CC-BY-SA-NC, so I've set that for now.
+
+[CC BY NC SA 4.0](LICENSE)
---
+++ bowlroll-english.user.js
@@ -0,0 +1,214 @@
+// ==UserScript==
+// @name         BowlRoll Translation
+// @namespace    https://github.com/vocauk/bowlroll-english
+// @version      0.1
+// @description  Makes BowlRoll much easier to use for non-Japanese speakers
+// @author       David (VOCA-UK TEAM)
+// @match        https://bowlroll.net/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    const baseURL = 'https://bowlroll.net/';
+
+    // profile
+    if (window.location.href.startsWith(baseURL + 'user/')) {
+      // edit profile
+      if (window.location.href.includes('profile')) {
+         document.title = 'Edit Profile - BowlRoll';
+         document.getElementById('page-nav-title').innerText = 'Edit Profile';
+         document.querySelector('.btn > span').innerText = 'Back to user page';
+
+         const nameForm = document.getElementById('user-profile-name');
+         nameForm.querySelector('label').innerText = 'Name';
+         nameForm.querySelector('span').innerText = 'Change';
+         nameForm.querySelector('.form-input-hint').innerText = 'Required, up to 24 characters';
+
+         const messageForm = document.getElementById('user-profile-message');
+         messageForm.querySelector('label').innerText = 'Message';
+         messageForm.querySelector('.form-input-hint').innerText = 'Up to 1024 characters';
+         messageForm.querySelector('.btn-primary').innerText = 'Change';
+         messageForm.querySelector('.btn-block').innerText = 'Reset';
+
+         const profileViewForm = document.getElementById('user-profile-view');
+         profileViewForm.querySelector('label').innerText = ('User Display: ' + (document.getElementById('user-profile-input-view').value === 'on' ? 'Public' : 'Hidden'));
+         document.getElementById('user-profile-view-public-btn').innerText = 'Public';
+         document.getElementById('user-profile-view-private-btn').innerText = 'Hidden';
+
+         const siteForm = document.getElementById('user-profile-site');
+         siteForm.querySelector('label').innerText = 'Site';
+         siteForm.querySelector('.btn-primary').innerText = 'Change';
+         siteForm.querySelector('.form-input-hint').innerText = 'URL notation only';
+
+         const countryForm = document.getElementById('user-profile-country');
+         countryForm.querySelector('label').innerText = 'Country';
+         countryForm.querySelector('.btn-primary').innerText = 'Change';
+         countryForm.querySelector('.form-input-hint').innerText = 'Up to 32 characters';
+
+         const locationForm = document.getElementById('user-profile-location');
+         locationForm.querySelector('label').innerText = 'Location';
+         locationForm.querySelector('.btn-primary').innerText = 'Change';
+         locationForm.querySelector('.form-input-hint').innerText = 'Up to 32 characters';
+      }
+
+      // this only appears on your own profile!
+      try {
+       document.querySelector('.outside > .h5').innerText = 'Display';
+       document.querySelector('.outside > .btn > span').innerText = 'Edit Profile';
+      } catch (e) {
+       // this only appears on other's!
+       document.querySelector('.tab-item > .badge').innerText = 'Latest Files';
+      }
+
+      document.querySelector('.tab-item > a').innerText = 'Home';
+      document.querySelector('.user-show-files > h3').innerText = 'Latest Files';
+    }
+
+
+    // file
+    // some of this page uses JS to get info, so it's not going to be possible to translate for a while
+    if (window.location.href.startsWith(baseURL + 'file/')) {
+      const share = document.getElementById('file-show-share-button');
+      share.querySelector('span').innerText = 'Share';
+      // this line below won't set - will look into later
+      document.querySelector('.modal-title').innerText = 'Share this page';
+      const shareModalText = document.querySelectorAll('.modal-body > dl > dt');
+      shareModalText[0].innerText = 'Share to other services';
+      shareModalText[1].innerText = 'Copy URL';
+      document.getElementById('file-show-copy-url-button').setAttribute('data-tooltip', 'Copied!');
+      const otherMenu = document.getElementById('file-show-other-menu');
+      otherMenu.querySelector('span').innerText = 'Report a problem (Japanese only)';
+      document.querySelector('.side-files > h5 > span').innerText = 'Other Files';
+
+      // neither will these
+      const editTags = document.getElementById('file-show-tags-modal-button');
+      editTags.querySelector('span').innerText = 'Edit Tags';
+      document.querySelector('.side-files > h5 > a > span').innerText = 'View all';
+      document.querySelector('.comments > h5 > span').innerText = 'Comments';
+      document.querySelector('section > h3').innerText = 'All latest submissions';
+    }
+
+    let descriptionsDonate, donateBoxes, titles, fileUploadTexts, fileUploadColumns;
+    switch (window.location.href) {
+        // homepage
+        case (baseURL):
+          titles = document.querySelectorAll('section > h3');
+
+          // hero
+          descriptionsDonate = document.querySelectorAll('.cover > div');
+          descriptionsDonate[0].innerText = 'Financial Support Request';
+          descriptionsDonate[1].innerText = 'BowlRoll needs financial support to continue operating.';
+          descriptionsDonate[2].innerText = 'If you\'d like to help out, please support us through the following services:';
+
+          donateBoxes = document.querySelectorAll('.donation-service');
+          donateBoxes[0].querySelector('p').innerText = 'If you would like to support us through FANBOX, please select your plan by clicking the button below.';
+          donateBoxes[0].querySelectorAll('p')[2].innerText = 'You can also scan the QR code on your mobile device.';
+          donateBoxes[1].querySelector('.h4').innerText = 'Creator Recommendation Program';
+          donateBoxes[1].querySelector('p').innerText = 'To view our current supporters, click on the button below.';
+          donateBoxes[1].querySelector('span').innerText = 'Content Tree';
+          donateBoxes[1].querySelectorAll('p')[2].innerText = 'When submitting work to Nico Nico Douga, please register the below work as a parent.'
+
+          titles[0].innerText = 'Notices';
+
+          // recent works
+          titles[1].innerText = 'Recent Submissions';
+
+          // users
+          titles[2].innerText = 'Contributing Users';
+
+          // related links
+          document.querySelector('.global-links > h3').innerText = 'Related Links';
+          break;
+
+        // settings
+        case (baseURL + 'setting/common'):
+           document.title = 'General Settings - BowlRoll';
+           document.getElementById('page-nav-title').textContent = 'General Settings';
+           document.querySelector('.form-label').textContent = 'Email';
+           // I'm not sure what this means!
+           document.querySelector('.form-input-hint').textContent = 'Email notation only';
+           document.querySelector('.input-group-btn > span').textContent = 'Submit';
+           document.querySelector('.label').textContent = 'Total file storage used';
+           break;
+
+        // help
+        case (baseURL + 'help/index'):
+           document.querySelector('.overview').innerHTML = `<h3>Help</h3>Currently, BowlRoll has no help section.
+           <br/>This has been in place sine the release of BowlRoll.<br/>We plan to create help documentation in the next version. In the meantime,
+           if you don't know how to use BowlRoll, just do your best!<br/>If you still don't understand, ask a question on BowlRoll's Twitter account
+           or contact us (Japanese only).`;
+           break;
+
+        // files
+        // doesn't seem to work properly atm
+        case (baseURL + 'file/index'):
+        case (baseURL + 'r18/file/index'):
+           document.title = 'File List - BowlRoll';
+           document.querySelector('.column > .btn > span').innerText = 'Upload';
+           document.querySelector('.h4 > span').innerText = 'Click here to upload';
+           document.querySelector('.common-btn').innerText = 'All Ages';
+           document.querySelector('.tab-item').innerText = 'Main Tags';
+
+           // sort
+           // The actual file listing system BowlRoll uses is written in JavaScript and loaded on the client-side, therefore
+           // we need to wait for it to get the initial page content first so it can render. It's late, so I'm not going to
+           // try injecting translations for now.
+           break;
+
+        // my approved files
+        case (baseURL + 'myself/approved-filelist'):
+           document.title = 'Approved Files - BowlRoll';
+           document.getElementById('page-nav-title').innerText = 'Approved Files';
+           // todo: sort
+           break;
+
+        // my submissions
+        case (baseURL + 'myself/filelist-public'):
+           document.title = 'Submitted Files - BowlRoll';
+           document.getElementById('page-nav-title').innerText = 'Submitted Files';
+           document.getElementById('tab-item-public').innerText = 'Public';
+           // Not too sure about these two translations
+           document.getElementById('tab-item-user').innerText = 'Specified User';
+           document.getElementById('tab-item-referer').innerText = 'Specified URL';
+
+           document.getElementById('tab-item-private').innerText = 'Private';
+           break;
+
+        // file upload
+        case (baseURL + 'file/upload'):
+           document.title = 'Upload - BowlRoll';
+           document.getElementById('page-nav-title').innerText = 'Upload';
+           document.querySelector('.btn > span').innerText = 'Back to File List';
+           fileUploadTexts = document.querySelectorAll('label > strong');
+           fileUploadTexts[0].innerText = 'Select file';
+           fileUploadTexts[1].innerText = 'Drop file here';
+           document.querySelector('.explanation > h5').innerText = 'File Submission Requirements';
+           fileUploadColumns = document.querySelectorAll('.explanation > .columns > .column');
+           fileUploadColumns[0].innerHTML = `<dl><dt>File Name</dt><dd>32 characters or less</dd><dd>(including extension)</dd>`;
+           fileUploadColumns[1].innerHTML = `<dl><dt>File Size</dt><dd>Less than 500MB</dd></dt>`;
+           fileUploadColumns[2].innerHTML = `<dl><dt>File Format</dt><dd><span>.zip</span><span>.rar</span><span>.7z</span></dd></dl>`;
+           break;
+
+        default:
+           break;
+    }
+
+    // sidebar
+    const sidebarItems = document.querySelectorAll('.menu-item > a > span');
+    const otherSidebarItems = document.querySelectorAll('nav > .contents > a');
+    sidebarItems[0].textContent = 'Files';
+    sidebarItems[1].textContent = 'Help';
+    sidebarItems[2].textContent = 'Contact Us (Japanese only)';
+    otherSidebarItems[0].textContent = 'About BowlRoll (Japanese only)';
+    otherSidebarItems[1].textContent = 'Terms of Use (Japanese only)';
+    otherSidebarItems[2].textContent = 'Privacy Policy (Japanese only)';
+
+    // user sidebar
+    sidebarItems[3].textContent = 'File Upload';
+    sidebarItems[4].textContent = 'My Submissions';
+    sidebarItems[5].textContent = 'Approved Submissions';
+    sidebarItems[6].textContent = 'Settings';
+    sidebarItems[7].textContent = 'Logout';
+})();
